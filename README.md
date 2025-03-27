2a9d0a531262ed99bf4f8fbe269913e837f15ede
soongao <gsong1984@163.com>
2025-03-27 22:05:15 +0800

    doc

--- README.md
+++ README.md
@@ -1,27 +1,29 @@
 # Easy Gin learning from scratch
-- 学习Go中Web框架的设计思路
+- 学习Go Web框架的设计思路
 - 学习net/http标准库
 - 实现了Gin框架的核心功能
+- 新添加了一个快速注册RESTful路由的方法
+  - 只需一条配置即可完成一套RESTful API注册
 
 ## 技术点
-1. 前缀树实现动态路由的绑定和解析
-2. 分组控制路由, Group Control
-3. 中间件插入点, c.Next()
-
-## 架构
-
-
+1. `前缀树`实现动态路由的绑定和解析
+2. 分组控制路由, `Group Control`
+3. `中间件`配置
+   - 自定义中间件
+   - c.Next()
+4. 新增快速注册`RESTful`路由的方法
+   - 用户只需实现RESTful interface即可完成RESTful api的注册
+  
 ## 项目结构
 ```text
 \---frame
-        context.go
-        engine.go
-        go.mod
-        logger.go
-        recover.go
-        router.go
-        routergroup.go
-        trie.go
+    context.go
+    engine.go
+    logger.go // logger中间件
+    recover.go // recover中间件
+    router.go 
+    routergroup.go
+    trie.go // 前缀树, 动态路由匹配
 ```
 
 ## 学习笔记
--- frame/routergroup.go
+++ frame/routergroup.go
@@ -16,6 +16,38 @@ func (rg *routerGroup) POST(pattern string, fc HandleFunc) {
 	rg.engine.router.addRoute("POST", rg.name+pattern, fc)
 }
 
+func (rg *routerGroup) PUT(pattern string, fc HandleFunc) {
+	rg.engine.router.addRoute("PUT", rg.name+pattern, fc)
+}
+
+func (rg *routerGroup) DELETE(pattern string, fc HandleFunc) {
+	rg.engine.router.addRoute("DELETE", rg.name+pattern, fc)
+}
+
 func (rg *routerGroup) Use(middlewares ...HandleFunc) {
 	rg.middlewares = append(rg.middlewares, middlewares...)
 }
+
+type RESTful interface {
+	Create(*Context)
+	Query(*Context)
+	Update(*Context)
+	Delete(*Context)
+}
+
+func (rg *routerGroup) REST(pattern string, rt RESTful) {
+	rg.GET(pattern, rt.Query)
+	rg.POST(pattern, rt.Create)
+	rg.PUT(pattern, rt.Update)
+	rg.DELETE(pattern, rt.Delete)
+}
+
+type BaseREST struct{}
+
+func (base *BaseREST) Create(c *Context) { c.JSON(200, "RESTful POST") }
+
+func (base *BaseREST) Query(c *Context) { c.JSON(200, "RESTful GET") }
+
+func (base *BaseREST) Update(c *Context) { c.JSON(200, "RESTful PUT") }
+
+func (base *BaseREST) Delete(c *Context) { c.JSON(200, "RESTful DELETE") }
